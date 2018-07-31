71898b4be36be5936e51d4137f1ed9143bc88e64
paolo <strlcopy@gmail.com>
2018-07-31 10:49:29 +0800

    custome markdown preview style

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -3,5 +3,12 @@
   (after-load 'whitespace-cleanup-mode
     (push 'markdown-mode whitespace-cleanup-mode-ignore-modes)))
 
+(setq markdown-command
+      (concat
+       "/home/paolo/anaconda3/bin/pandoc"
+       " --from=markdown --to=html"
+                                        ;" --standalone --css=/home/paolo/.emacs.d/markdown-css/github-pandoc.css --self-contained --mathjax=/home/paolo/.emacs.d/markdown-css/MathJax.js?config=TeX-AMS_CHTML-full --highlight-style=pygments"
+       " --standalone --css=/home/paolo/.emacs.d/markdown-css/github-pandoc.css --self-contained --webtex --highlight-style=pygments"
+       ))
 
 (provide 'init-markdown)
